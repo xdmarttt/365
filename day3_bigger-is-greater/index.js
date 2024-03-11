2b123215237332f077f83fcf83f5f596e87429ad
Mart Macion <martsonic901@gmail.com>
2024-03-11 09:49:45 +0800

    :sparkles: finalize bbigger is greater solution

--- day3_bigger-is-greater/index.js
+++ day3_bigger-is-greater/index.js
@@ -1,17 +1,32 @@
 // Unfinished solution
 // https://www.hackerrank.com/challenges/bigger-is-greater/problem
 function biggerIsGreater(w) {
-    let origChar = w;
-    let lessChar = 'z'.repeat(100);
+    let firstStop = -1;
+    let nextHighestStrInd = -1;
+    const wordArr = w.split("");
     
-    for(let i = w.length - 1; i >= 0; i--) {
-        for (let j = w.length -1; j>= 0; j--) {
-            const swapedChar = swapCharacters(lessChar === 'z'.repeat(100) ? w : lessChar, i, j);
-            if (swapedChar > origChar && swapedChar < lessChar) {
-                lessChar = swapedChar;
-            }
+    for (let i = 0; i < w.length; i++) {
+        if (wordArr[i] < wordArr[i+1]) {
+            firstStop = i; 
         }
     }
     
-    return lessChar === 'z'.repeat(100) ? 'no answer' : lessChar;
+    if (firstStop === -1) {
+        return 'no answer';
+    }
+    
+    for (let i = firstStop+1; i < w.length; i++) {
+        if (wordArr[firstStop] < wordArr[i]) {
+            nextHighestStrInd = i; 
+        }
+    }
+    
+    let tmp = wordArr[firstStop];
+    wordArr[firstStop] = wordArr[nextHighestStrInd];
+    wordArr[nextHighestStrInd] = tmp;
+    
+    let leftPart = wordArr.slice(0, firstStop + 1);
+    let rightPart = wordArr.slice(firstStop + 1).sort();
+
+    return [...leftPart, ...rightPart].join('');
 }
